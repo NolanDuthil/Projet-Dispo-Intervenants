1c49f11d5d836997d33c7623646753311c7e42d9
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2024-11-18 09:48:38 +0100

    login form

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,6 @@
-export type User = {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-  };
+// export type User = {
+//     id: string;
+//     name: string;
+//     email: string;
+//     password: string;
+//   };
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,15 +8,15 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useActionState } from 'react';
-import { authenticate } from '@/app/lib/actions';
+// import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined,
-  );
+  // const [errorMessage, formAction, isPending] = useActionState(
+  //   authenticate,
+  //   undefined,
+  // );
   return (
-    <form action={formAction} className="space-y-3">
+    <form /*action={formAction}*/ className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`mb-3 text-2xl`}>
           Please log in to continue.
@@ -62,7 +62,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
+        <Button className="mt-4 w-full" /*aria-disabled={isPending}*/>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div
@@ -70,12 +70,12 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
-          {errorMessage && (
+          {/* {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
-          )}
+          )} */}
         </div>
       </div>
     </form>
